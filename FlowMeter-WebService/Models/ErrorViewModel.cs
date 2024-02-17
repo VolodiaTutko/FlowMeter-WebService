35d44adbd337ff60b98cda620bed986d860d1a5e
VolodiaTutko <volodatutko32@gmail.com>
2024-02-17 18:05:40 +0200

    Add project files.

---
+++ FlowMeter-WebService/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FlowMeter_WebService.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
