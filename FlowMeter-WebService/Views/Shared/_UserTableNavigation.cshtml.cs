e1dcaf6522b2d0a772bbfdc4fc5208e9db87162f
ayatsuliak <andrii.yatsulaik@gmail.com>
2024-03-29 16:33:54 +0200

    read, update, delete services

---
+++ FlowMeter-WebService/Views/Shared/_UserTableNavigation.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FlowMeter_WebService.Views.Shared
+{
+    public class _UserTableNavigationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
